f8422e749bd113b00731d418607f4b64366927af
Rebecca Still <beckystill@hotmail.com>
2020-03-13 20:57:14 +0000

    add images to inventory

--- frontend/dino-park/src/components/inventory/Inventory.jsx
+++ frontend/dino-park/src/components/inventory/Inventory.jsx
@@ -5,10 +5,22 @@ const Inventory = () => {
   return (
     <>
       <div className="inventory-container">
-        <div className="inventory-item-1"><div className="item-count">0</div></div>
-        <div className="inventory-item-2"><div className="item-count">0</div></div>
-        <div className="inventory-item-3"><div className="item-count">0</div></div>
-        <div className="inventory-item-4"><div className="item-count">0</div></div>
+        <div className="inventory-item-1">
+          <img className="inv-img" src={require("../../images/Sprite-Meat.png")} alt="raw meat"/>
+          <div className="item-count">0</div>
+        </div>
+        <div className="inventory-item-2">
+          <img className="inv-img" id="mushroom" src={require("../../images/Sprite400.png")} alt="mushroom"/>
+          <div className="item-count">0</div>
+        </div>
+        <div className="inventory-item-3">
+          <img className="inv-img" src={require("../../images/Sprite-egg.png")} alt="dinosaur egg"/>
+          <div className="item-count">0</div>
+        </div>
+        <div className="inventory-item-4">
+          <img className="inv-img" src={require("../../images/Sprite-medicine.png")} alt="medicine"/>
+          <div className="item-count">0</div>
+        </div>
       </div>
     </>
   );
--- frontend/dino-park/src/components/shop/Shop.jsx
+++ frontend/dino-park/src/components/shop/Shop.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import Inventory from '../inventory/Inventory';
+import './Shop.css';
 
 const Shop = () => {
   return (
     <>
       <div className="shop-grid">
+        <div className="shop-header">
+          <Inventory/>
+          <h1>Supplies</h1>
+        </div>
+        <div className="item-grid">
+          <div classname="item" id="item-1">Mushroom</div>
+          <div classname="item" id="item-2">Raw Meat</div>
+          <div classname="item" id="item-3">Medi-kit</div>
+          <div classname="item" id="item-4">Level 1 Egg</div>
+          <div classname="item" id="item-5">Level 2 Egg</div>
+          <div classname="item" id="item-6">Level 3 Egg</div>
+        </div>
+        <div className="item-details">
+          <h2>Item Name</h2>
+          <p>Item Description</p>
+          <p>Item Price</p>
+        </div>
       </div>
     </>
   );
