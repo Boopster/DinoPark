7e89cc3b0bde696e3cd8d4a51a5eef5a022eb74d
Rebecca Still <beckystill@hotmail.com>
2020-03-15 08:07:33 +0000

    add DinosaurTests

--- backend/dino-park/src/test/java/com/personal/example/dinopark/DinoParkApplicationTests.java
+++ backend/dino-park/src/test/java/com/personal/example/dinopark/DinoParkApplicationTests.java
@@ -1,6 +1,5 @@
 package com.personal.example.dinopark;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ backend/dino-park/src/test/java/com/personal/example/dinopark/models/DinosaurTests.java
@@ -0,0 +1,66 @@
+package com.personal.example.dinopark.models;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DinosaurTests {
+
+    Dinosaur dinosaur;
+
+    @Before
+    public void setUp() {
+        dinosaur = new Dinosaur("Diplodocus", "Herbivore");
+    }
+
+    @Test
+    public void hasSpecies() {
+        assertEquals("Diplodocus", dinosaur.getSpecies());
+    }
+
+    @Test
+    public void canSetSpecies() {
+        dinosaur.setSpecies("T-Rex");
+        assertEquals("T-Rex", dinosaur.getSpecies());
+    }
+
+    @Test
+    public void hasDiet() {
+        assertEquals("Herbivore", dinosaur.getDiet());
+    }
+
+    @Test
+    public void canSetDiet() {
+        dinosaur.setDiet("Carnivore");
+        assertEquals("Carnivore", dinosaur.getDiet());
+    }
+
+    @Test
+    public void hasHealth() {
+        assertEquals(10, dinosaur.getHealth());
+    }
+
+    @Test
+    public void canSetHealth() {
+        dinosaur.setHealth(9);
+        assertEquals(9, dinosaur.getHealth());
+    }
+
+    @Test
+    public void hasAge() {
+        assertEquals(0, dinosaur.getAge());
+    }
+
+    @Test
+    public void canSetAge() {
+        dinosaur.setAge(1);
+        assertEquals(1, dinosaur.getAge());
+    }
+
+}
