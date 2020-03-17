b83292dc44467f6d963da961542e6a47a3e7d39b
Rebecca Still <beckystill@hotmail.com>
2020-03-17 18:10:44 +0000

    fix create game to add correct balance

--- backend/dino-park/src/main/java/com/personal/example/dinopark/controllers/GameController.java
+++ backend/dino-park/src/main/java/com/personal/example/dinopark/controllers/GameController.java
@@ -3,25 +3,17 @@ package com.personal.example.dinopark.controllers;
 import com.personal.example.dinopark.models.Game;
 import com.personal.example.dinopark.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value="/api")
+@RequestMapping(value="/games")
 public class GameController {
 
     @Autowired
     GameRepository gameRepository;
-
-    @GetMapping(value = "/games")
-    public List<Game> getAllGames() {
-        return gameRepository.findAll();
-    }
 
     @GetMapping("{id}")
     public Optional<Game> getGame(@PathVariable Long Id) {
